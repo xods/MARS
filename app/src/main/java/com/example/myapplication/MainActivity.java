44262dbb29783163fabb79c2d23792fbfdda35cc
xods <obradovic.rade07@gmail.com>
2023-04-02 19:36:32 +0200

    Izmenjena main aktivnost i promenjena ikonica app.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim_txt);
+
+        TextView txtLogo = findViewById(R.id.txtVLogo);
+        Button login = findViewById(R.id.btnLogIn);
+        Button signIn = findViewById(R.id.btnSignIn);
+        Button play = findViewById(R.id.btnPlayGuest);
+
+        txtLogo.setAnimation(animation);
+        login.setAnimation(animation);
+        signIn.setAnimation(animation);
+        play.setAnimation(animation);
     }
 }
--- app/src/main/java/com/example/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -3,7 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
@@ -18,6 +20,14 @@ public class SplashActivity extends AppCompatActivity {
         TextView txtLogo = findViewById(R.id.txtVLogo);
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim_txt);
         txtLogo.setAnimation(animation);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        },5000L);
     }
 
     @Override
