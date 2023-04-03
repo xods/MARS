32a17d3773ea108edf94dc898e08395df3a2009b
xods <obradovic.rade07@gmail.com>
2023-04-04 01:39:06 +0200

    Dodat activity za igru ko zna zna.

---
+++ app/src/main/java/com/example/myapplication/activity/KoZnaZnaActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.myapplication.R;
+
+public class KoZnaZnaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ko_zna_zna);
+
+    }
+}
